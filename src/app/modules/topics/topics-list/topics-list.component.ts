356067fff8e51d8e4a2db5852fdee74a26fd4585
Akhil Baddi <akhilbaddi@surnoi.com>
2025-03-14 23:14:32 +0530

    Added project structure with shared, core, and modules componen

---
+++ src/app/modules/topics/topics-list/topics-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topics-list',
+  imports: [],
+  templateUrl: './topics-list.component.html',
+  styleUrl: './topics-list.component.scss'
+})
+export class TopicsListComponent {
+
+}
